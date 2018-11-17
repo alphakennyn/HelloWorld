acd61c7933893d8ac06462db6cb3535274ef17ed
Johnny97 <johnny.cl97@gmail.com>
2018-11-17 15:33:09 -0500

    pull from andy

---
+++ client/src/components/SpeechRecognition.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    {{data}}
+    <button type = "button">Speak</button>
+  </div>
+</template>
+
+<script>
+import request from '../api/requests.js';
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      data: {}
+    }
+  },
+  props: {
+    msg: String
+  },
+  mounted: function() {
+    request.getSample().then(res => {
+      this.data = res.data;
+    }).catch(err => {
+      alert('uh oh');
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
