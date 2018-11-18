f68daf7116cde1120ec4910329786343993fc3ca
Johnny97 <johnny.cl97@gmail.com>
2018-11-17 22:22:38 -0500

    ui commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id = "bj">
+     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #bj{
+  
+  position: absolute;
+	top: -20px;
+	left: -20px;
+	right: -40px;
+	bottom: -40px;
+	width: auto;
+	height: auto;
+	background-image: url('./assets/sky.jpg');
+	background-size: cover;
+ 
+	/* -webkit-filter: blur(5px); */
+	z-index: 0;
+    }
+
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Homepage from './components/Homepage.vue'
+import chat from './components/chat.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Homepage },
+  { path: '/chat', component: chat }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
